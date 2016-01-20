dcfc2f3c73e406851cde5b9cb16af6a7317691a4
Yuecheng Guo <gyc8990@gmail.com>
2016-01-20 15:33:19 -0500

    revised the input args of main()

--- src/Assignment1/Q15.java
+++ src/Assignment1/Q15.java
@@ -7,7 +7,7 @@ package Assignment1;
 import java.io.*;
 
 public class Q15 {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		String str = "I";
 		nestedTryBlock(str);
 	}
