d681dbd2bd5d175fcdf988639ca40f79f7a25b4e
Yuecheng Guo <gyc8990@gmail.com>
2016-01-23 21:53:28 -0500

    setup A2Q1(JavaIO)

---
+++ src/Assignment2/JavaIOQ2.java
@@ -0,0 +1,12 @@
+package Assignment2;
+
+/*
+ * create a GUI program using Java Swing:
+ * Display a Text area for input paragraph. 
+ * On click of a button, save the file in a .txt file.
+ */
+
+
+public class JavaIOQ2 {
+	
+}
