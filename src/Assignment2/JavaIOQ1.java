aa95b25d8d773751559ccb965e9dfd4b41712593
Yuecheng Guo <gyc8990@gmail.com>
2016-01-23 21:48:28 -0500

    initialization of assignment 2

---
+++ src/Assignment2/JavaIOQ1.java
@@ -0,0 +1,45 @@
+package Assignment2;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class JavaIOQ1 {
+	/* declare constants */
+	/** file directory */
+	private static final String FILEPATH = "/User/Yuecheng/Desktop/";
+	
+	/** file type to be filtered */
+	private static String FILENAME = "text1.txt"; 
+	
+	public static void main(String[] args) {
+		JavaIOQ1 instance = new JavaIOQ1();
+		try {
+			File f = new File(FILENAME);
+			instance.fileFilter(f);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void fileFilter(File f){
+		FilenameFilter ff = new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String s) {
+				if (s.lastIndexOf('.') > 0){
+					int ind = s.lastIndexOf('.'); // index of . in the string
+					String str = s.substring(ind); // save the extension in str
+					if (str.equals(".txt")) {
+						return true;
+					}
+				} else {
+					System.out.println("Please re-enter the file type you want.e.g.: filter.txt");
+				}
+				return false;
+			}
+		};
+		File[] paths = f.listFiles(ff);
+		for (File path:paths) {
+			System.out.println(path);
+		}
+	}
+}
