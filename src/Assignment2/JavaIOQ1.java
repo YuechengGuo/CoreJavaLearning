9e4841321ebd713bc463bd5375685a6c0ffca4ae
Yuecheng Guo <gyc8990@gmail.com>
2016-01-25 02:25:43 -0500

    revised the filter code to be easily reused

--- src/Assignment2/JavaIOQ1.java
+++ src/Assignment2/JavaIOQ1.java
@@ -13,28 +13,27 @@ public class JavaIOQ1 {
 	private static final String FILEPATH = "/Users/Yuecheng/Desktop/";
 	
 	/** file type to be filtered */
-	private static String FILEEXTENTSION= ".txt"; 
+	private static String FILEEXTENSION= ".txt"; 
+	
+	JavaIOQ1() {
+//		this.filePath = path;
+//		this.fileExtension = extension;
+	}
 	
 	public static void main(String[] args) {
 		JavaIOQ1 instance = new JavaIOQ1();
-		try {
-			File f = new File(FILEPATH);
-//			System.out.println(f);
-			instance.fileFilter(f);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
+		System.out.println(instance.displayFiles(FILEPATH, FILEEXTENSION));
 	}
 	
 	/* filter main method, override accept(File dir, String name){} method */
-	public void fileFilter(File f){
+	public String[] fileFilter(File f, String ext){
 		FilenameFilter fileNameFilter = new FilenameFilter() {
 			@Override
 			public boolean accept(File dir, String name) {
 				if (name.lastIndexOf('.') > 0){
 					int ind = name.lastIndexOf('.'); // index of . in the string
 					String str = name.substring(ind); // save the extension in str
-					if (str.equals(FILEEXTENTSION)) {
+					if (str.equals(ext)) {
 						return true;
 					}
 				}
@@ -42,8 +41,22 @@ public class JavaIOQ1 {
 			}
 		};
 		File[] paths = f.listFiles(fileNameFilter);
-		for (File path:paths) {
-			System.out.println(path);
+		String[] output = new String[paths.length];
+		for (int i = 0; i < paths.length; i++) {
+			output[i] = paths[i].getPath();
 		}
+		return output;
 	}
+	
+	public String[] displayFiles(String path, String ext) {
+		try {
+			File f = new File(path);
+			String[] str = fileFilter(f, ext);
+			return str;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
+	
 }
