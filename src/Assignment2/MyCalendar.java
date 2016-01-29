f8811b69115524c9d67f181182c5db70ceba75e3
Yuecheng Guo <gyc8990@gmail.com>
2016-01-29 17:56:27 -0500

    A2Calendar finished

--- src/Assignment2/MyCalendar.java
+++ src/Assignment2/MyCalendar.java
@@ -1,25 +1,36 @@
 package Assignment2;
 
 import java.awt.BorderLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Date;
 import java.util.Calendar;
 import java.util.HashMap;
 
 import javax.swing.*;
 
-public class MyCalendar {
+public class MyCalendar implements ActionListener {
 	
-	private static final int WIDTH = 400;
-	private static final int HEIGHT = 400;
+	private static final int WIDTH = 280;
+	private static final int HEIGHT = 240;
 	
-	private static final int DISPLAYWIDTH = WIDTH / 3 * 2;
-	private static final int DISPLAYHEIGHT = HEIGHT / 3 * 2;
+	private static final int XOFFSET = WIDTH / 20;
+	private static final int YOFFSET = HEIGHT / 20;
+	
+	private static final int DISPLAYWIDTH = WIDTH - 2 * XOFFSET;
+	private static final int DISPLAYHEIGHT = 120;
+	
+	private static final int YPADDING = 10;
+	private static final int XPADDING = WIDTH / 20 * 0;
 	
 	private static final int NDAYSAWEEK = 7;
 	private static final int WEEKSINTABLE = 6;
 	
+	private static final int DEFAULTYEAR = 2016;
+	private static final int DEFAULTMONTH = 1;
 	
-	
+
 	MyCalendar() {
 		initialization();
 		getDate();
@@ -31,17 +42,38 @@ public class MyCalendar {
 		
 		weekStrToNum = new HashMap<String, Integer>();
 		
-		date = new String[WEEKSINTABLE][NDAYSAWEEK];//{{"1", "2" ,"3" ,"1" ,"2" ,"3" ,"1"}};
-		cal = Calendar.getInstance();
+		date = new String[WEEKSINTABLE][NDAYSAWEEK];
 		days = new String[]{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+		cal = Calendar.getInstance();
+		
 		int count = 0;
 		for (String str:days){
 			weekStrToNum.put(str, count);
 			count++;
 		}
 		
-		calendarTable = new JTable(date, days);
+		year = DEFAULTYEAR;
+		month = DEFAULTMONTH;
 		
+		calendarTable = new JTable(date, days);
+		calendarTable.setSize(DISPLAYWIDTH, DISPLAYHEIGHT);
+		
+		cbMonthModel = new DefaultComboBoxModel<Integer>();
+		cbMonthModel.setSelectedItem(DEFAULTMONTH);
+		cbMonth = new JComboBox<Integer>(cbMonthModel);
+		selectMonth();
+		cbMonth.addActionListener(this);
+		cbMonth.setSize(100, 50);
+		 frame.add(cbMonth);
+		
+		cbYearModel = new DefaultComboBoxModel<Integer>();
+		cbYearModel.setSelectedItem(DEFAULTYEAR);
+		cbYear = new JComboBox<Integer>(cbYearModel);
+		selectYear();
+		cbYear.addActionListener(this);
+		cbYear.setSize(100, 50);
+		frame.add(cbYear);
+
 		
 		calendarDisplay = new JScrollPane(calendarTable);
 		calendarDisplay.setSize(DISPLAYWIDTH, DISPLAYHEIGHT);
@@ -52,25 +84,62 @@ public class MyCalendar {
 		frame.setLayout(new BorderLayout());
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 	
 	private void customizeLayout() {
+		Insets insets = frame.getInsets();
+		calendarDisplay.setBounds(XOFFSET + insets.left, YOFFSET + insets.top, calendarTable.getWidth(), calendarTable.getHeight());
+		cbYear.setBounds(XOFFSET + insets.left, YOFFSET + calendarTable.getHeight() + YPADDING + insets.top, cbYear.getWidth(), cbYear.getHeight());
+		cbMonth.setBounds(XOFFSET + cbYear.getWidth() + XPADDING + insets.left, YOFFSET + calendarTable.getHeight() + YPADDING + insets.top, cbMonth.getWidth(), cbMonth.getHeight());
 		
 	}
 	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == cbMonth) {
+			month = (int) cbMonth.getSelectedItem();
+			getDate();
+		} else if (e.getSource() == cbYear) {
+			year = (int) cbYear.getSelectedItem();
+			getDate();
+		}
+	}
+	
+	private void selectMonth() {
+		for (int i = 1; i < 13; i++) {
+			cbMonthModel.addElement(i);
+		}
+	}
+	
+	private static void selectYear() {
+		for (int i = 1900; i < 2100; i++) {
+			cbYearModel.addElement(i);
+		}
+	}	
+	
 	private void getDate() {
-		year = 2016;
-		month = 2;
+		calendarTable.repaint();
 		cal.setTime(new Date(year - 1900, month - 1, 1)); // set time to the first day of selected month
 		String[] str = cal.getTime().toString().split(" ");
 		int maxNDays = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
-		createMonthTable(str[0], maxNDays); //str[0]weekday; str[1]month; str[2]date; str[3]time; str[4]timeZone; str[5]year 
+		createMonthTable(str[0], maxNDays); //str[0]weekday; str[1]month; str[2]date; str[3]time; str[4]timeZone; str[5]year
+//		// test display start
+//		System.out.println("Weekday:" + str[0] + ". Month: " + str[1] + ". Date: " + str[2] + ". Year: " + str[5]);
+//		for (int i = 0; i < date.length; i++) { 
+//			String rowElement = "";
+//			for (int j = 0; j < date[i].length; j++) {
+//				rowElement += date[i][j] + ", ";
+//			}
+//			System.out.println("row " + i + ": " + rowElement);
+//		}
+//		// test display done
 	}
+	
+
 	
 	private void createMonthTable(String weekday, int maxNDays) {
 		int firstDayIndex = weekStrToNum.get(weekday); 
-		System.out.println(firstDayIndex);
-		
 		for (int i = 0; i < NDAYSAWEEK * WEEKSINTABLE; i ++) {
 			int[] LMNMY = new int[3];
 			int ind;
@@ -82,7 +151,6 @@ public class MyCalendar {
 				addDayToTable(i, ind, LMNMY[0]);
 			} else if (i >= firstDayIndex + maxNDays) {
 				cal.setTime(new Date(LMNMY[2] - 1900, LMNMY[1] - 1, 1)); 
-				int lastMonthMaxNDays = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
 				ind = i - firstDayIndex - maxNDays;
 				addDayToTable(i, ind, LMNMY[0]);
 			} else {
@@ -132,11 +200,19 @@ public class MyCalendar {
 	private static JTable calendarTable;
 	private static JScrollPane calendarDisplay;
 	
+	private static DefaultComboBoxModel<Integer> cbMonthModel;
+	private static JComboBox<Integer> cbMonth;
+	
+	private static DefaultComboBoxModel<Integer> cbYearModel;
+	private static JComboBox<Integer> cbYear;
+	
 	private static int year;
 	private static int month;
-//	private static int lastMonth;
-//	private static int nextMonth;
 	
 	private static HashMap<String, Integer> weekStrToNum;
+
+
+
+	
 	
 }
